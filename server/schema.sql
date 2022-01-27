fd252bd1fdd66c1bb20979e590da60ef6dc06e5f
Bulgan Erdenebaatar <bulganebaatar@gmail.com>
2022-01-27 11:02:34 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,27 @@
+DROP DATABASE IF EXISTS `chat`;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
-
 /* Create other tables and define schemas for them here! */
 
+DROP TABLE IF EXISTS `users`;
 
+CREATE TABLE users (
+  `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  `username` TEXT
+);
 
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE messages (
+  `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  `text` TEXT,
+  `user` INT NOT NULL,
+  `roomname` TEXT,
+  FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
